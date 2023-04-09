c8080920c9a2b6dc82fab5f4a91c81057ad47eac
xConde <edjconde@live.com>
2023-04-08 21:31:07 -0500

    finish cow tip interaction & update interact

--- cow_interaction.py
+++ cow_interaction.py
@@ -1,22 +1,24 @@
 import random
 
 class CowInteraction():
-    def __init__(self, player, cow):
+    def __init__(self, game_instance, player, cow):
+        self.game_instance = game_instance
         self.player = player
         self.cow = cow
 
     def interact(self):
-        combat_chance = self.calculate_combat_chance()
-        dairy_chance = self.calculate_dairy_chance()
-
-        action_roll = random.randint(1, 100)
-
-        if action_roll <= combat_chance:
-            self.handle_combat()
-        elif action_roll <= combat_chance + dairy_chance:
-            self.handle_dairy()
-        else:
-            self.handle_tip_or_leave()
+        handlers = {
+            "aggro": self.handle_combat,
+            "shop": self.handle_shop,
+            "dairy": self.handle_dairy,
+            "tip_or_leave": self.handle_tip_or_leave
+        }
+        
+        cow_type = "shop" if self.cow.is_shop else "dairy" if self.cow.is_dairy else "aggro" if self.cow.is_aggro else "tip_or_leave"
+        handler = handlers[cow_type]
+        
+        self.game_instance.player.display_info(combat=self.cow.is_aggro)
+        handler()
 
     def calculate_combat_chance(self):
         base_chance = 15
@@ -52,15 +54,18 @@ class CowInteraction():
         if self.player.hp <= 0:
             print("You have been defeated by the cow.")
 
+    def handle_shop(self):
+        print('shopping!')
+
     def handle_dairy(self):
         bucket = self.get_bucket()
         if bucket:
             liquid_gold = bucket.use()
             self.player.update_inventory(liquid_gold, "add")
             self.player.update_inventory(bucket, "remove")
-            print(self.cow.get_dairy_response(True))
+            print(self.cow.print_response(self.cow.name, 'dairy_bucket'))
         else:
-            print(self.cow.get_dairy_response(False))
+            print(self.cow.print_response(self.cow.name, 'dairy_no_bucket'))
 
     def get_cow_bell(self):
         for item in self.player.inventory:
@@ -75,5 +80,46 @@ class CowInteraction():
         return None
 
     def handle_tip_or_leave(self):
-        # Handle tipping or leaving interaction with self.cow
-        pass
+        self.cow.print_response(self.cow.name, 'intro')
+        attempts = 2
+        total_tip = 0
+        while attempts > 0:
+            print("1. Tip the cow")
+            print("2. Run")
+
+            choice = input("Choose an action (1-2): ")
+
+            if choice == "1":
+                try:
+                    tip_amount = float(input("How much do you want to tip the cow? "))
+                    if (tip_amount <= 0):
+                        print("You decided not to tip the cow.")
+                        attempts = -1
+                    else:
+                        tip_amount = float(tip_amount)
+                        self.player.cash -= tip_amount
+                        total_tip += tip_amount
+
+                    if tip_amount >= self.cow.req_amount:
+                        score = (2 if attempts == 2 else 1) * (2 if tip_amount > 20 else 1)
+                        self.game_instance.update_cow_scores(self.cow, score)
+                        self.cow.likeliness += score
+                        print(self.cow.print_response(self.cow.name, 'graceful'))
+                        return
+                    else:
+                        attempts -= 1
+                        if (attempts == 0):
+                            self.game_instance.update_cow_scores(self.cow, -1)
+                            self.cow.likeliness -= 1
+                        tip_amount = float(tip_amount)
+                        self.player.cash -= tip_amount
+                        print(f'The cow is disappointed with your tip. You have ${int(self.player.cash)} left.')
+                        self.cow.print_response(self.cow.name, 'counter')
+                except ValueError:
+                    print("Please enter a number.")
+            elif choice == "2":
+                print("You decided to run away from the cow.")
+                self.game_instance.update_cow_scores(self.cow, -2)
+                return
+            else:
+                print("Please enter a number between 1 and 2.")
