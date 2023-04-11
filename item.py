20601f82753c14644bb9aadbc21eab124b987229
xConde <edjconde@live.com>
2023-04-10 22:21:04 -0500

    add tier weighted random (combat) item builder

--- item.py
+++ item.py
@@ -11,6 +11,10 @@ class Weapon(Item):
         self.min_damage = min_damage
         self.max_damage = max_damage
         self.rarity = rarity
+        self.type = 'combat'
+
+    def stats(self):
+        return f"{self.name} (L: {self.min_damage}, H: {self.max_damage}, Rarity: {self.rarity})"
 
 class Shield(Item):
     def __init__(self, name, min_defense, max_defense, rarity):
@@ -18,11 +22,16 @@ class Shield(Item):
         self.min_defense = min_defense
         self.max_defense = max_defense
         self.rarity = rarity
+        self.type = 'combat'
+
+    def stats(self):
+        return f"{self.name} (L: {self.min_defense}, H: {self.max_defense}, Rarity: {self.rarity})"
 
 class Tool(Item):
     def __init__(self, name, action):
         super().__init__(name, 'tool')
         self.action = action
+        self.type = 'utility'
 
 class CowBell(Tool):
     def __init__(self):
@@ -42,6 +51,7 @@ class Object(Item):
     def __init__(self, name, description):
         super().__init__(name, 'object')
         self.description = description
+        self.type = 'product'
 
 class LiquidGold(Object):
     def __init__(self):
@@ -81,7 +91,7 @@ SHIELD_TYPES = [
     {'name': 'aegis', 'min_def': 10, 'max_def': 22}
 ]
 
-rarity_adjectives = {
+RARITY_ADJECTIVES = {
     'common': {
         'prefix': ['Plain', 'Simple', 'Basic'],
         'suffix': ['']
@@ -105,61 +115,31 @@ rarity_adjectives = {
 }
 
 def get_modified_name(base_name, rarity, item_type):
-    if item_type not in ['weapon', 'shield']:
-        raise ValueError("Invalid item_type: must be 'weapon' or 'shield'")
-
     prefix_or_suffix = random.choice(['prefix', 'suffix'])
     adjective = random.choice(RARITY_ADJECTIVES[rarity][prefix_or_suffix])
     return f'{adjective} {base_name}'.strip() if prefix_or_suffix == 'prefix' else f'{base_name} {adjective}'.strip()
 
-def random_weapon_roll(less_likely=False):
-    weapons = [
-        {'name': 'dagger', 'min_dmg': 1, 'max_dmg': 10, 'max_scale': 3},
-        {'name': 'short bow', 'min_dmg': 2, 'max_dmg': 15, 'max_scale': 2},
-        {'name': 'sword', 'min_dmg': 3, 'max_dmg': 25, 'max_scale': 4}
-    ]
-    weapon = random.choice(weapons)
-    weights = [50, 30, 15, 5] if not less_likely else [75, 20, 4, 1]
-    scale = random.choices([1, 2, 3, 4], weights=weights)[0]
-    max_damage = min(weapon['max_dmg'], weapon['min_dmg'] + scale)
-    rarity = scale
-    return Weapon(weapon['name'], weapon['min_dmg'], max_damage, rarity)
+def build_item(item_type: str, less_likely: bool = False):
+    if item_type not in ['weapon', 'shield', 'tool', 'object']:
+        raise ValueError('Invalid item type.')
 
+    item_list = WEAPON_TYPES if item_type == 'weapon' else SHIELD_TYPES
+    item_weights = [35, 28, 24, 15, 12, 9, 6, 4, 2, 1] if not less_likely else [40, 30, 20, 10, 5, 3, 2, 1, 1, 1]
+    item_index = random.choices(range(len(item_list)), weights=item_weights)[0]
+    base_item = item_list[item_index]
 
-def random_shield_roll(less_likely=False):
-    shields = [
-        {'name': 'wooden shield', 'min_def': 1, 'max_def': 5, 'max_scale': 3},
-        {'name': 'funny shield', 'min_def': 2, 'max_def': 10, 'max_scale': 3},
-        {'name': 'hilarious shield', 'min_def': 4, 'max_def': 12, 'max_scale': 4}
-    ]
-    shield = random.choice(shields)
-    weights = [50, 30, 15, 5] if not less_likely else [75, 20, 4, 1]
-    scale = random.choices([1, 2, 3, 4], weights=weights)[0]
-    max_defense = min(shield['max_def'], shield['min_def'] + scale)
-    rarity = scale
-    return Shield(shield['name'], shield['min_def'], max_defense, rarity)
+    weights = [60, 35, 10, 4, 1] if not less_likely else [72, 32, 3, 2, 1]
+    scale = random.choices([1, 2, 3, 4, 5], weights=weights)[0]
+    rarity = list(RARITY_ADJECTIVES.keys())[scale - 1]
 
-def random_tool_roll():
-    tools = [CowBell(), Bucket()]
-    return random.choice(tools)
+    stat_key = 'min_dmg' if item_type == 'weapon' else 'min_def'
+    min_stat = base_item[stat_key]
+    max_stat = min(base_item['max_' + stat_key[-3:]], min_stat + scale)
+    
+    item = Weapon(base_item['name'], min_stat, max_stat, rarity) if item_type == 'weapon' else Shield(base_item['name'], min_stat, max_stat, rarity)
+    item.name = get_modified_name(item.name, rarity, item_type)
 
-def random_object_roll():
-    objects = [LiquidGold()]
-    return random.choice(objects)
-
-def random_item_roll():
-    item_type = random.choices(
-        ['weapon', 'shield', 'tool', 'object'],
-        weights=[50, 34, 15, 1]
-    )[0]
-    if item_type == 'weapon':
-        return random_weapon_roll(less_likely=True)
-    elif item_type == 'shield':
-        return random_shield_roll(less_likely=True)
-    elif item_type == 'tool':
-        return random_tool_roll()
-    else:
-        return random_object_roll()
+    return item
 
 def roll_weapon_dmg(weapon) -> int:
     if not weapon:
@@ -168,3 +148,39 @@ def roll_weapon_dmg(weapon) -> int:
     min_dmg = weapon.min_dmg + int(weapon.min_dmg * weapon.scale * 0.1)
     max_dmg = weapon.max_dmg + int(weapon.max_dmg * weapon.scale * 0.15)
     return random.randint(min_dmg, max_dmg)
+
+
+def random_item_roll(less_likely):
+    weights=[35, 30, 32, 3] if not less_likely else [63, 15, 20, 2]
+    item_type = random.choices(['weapon', 'shield', 'tool', 'object'],weights=weights)[0]
+    if item_type == 'weapon':
+        return build_item('weapon', less_likely)
+    elif item_type == 'shield':
+        return build_item('shield', less_likely)
+    elif item_type == 'tool':
+        return random_tool_roll()
+    else:
+        return random_object_roll()
+
+def get_shop_items(cow_mood: str, player_cash: float, isLucky: bool):
+    price = random.randint(20, 25) + 3 * int(player_cash // 50)
+    price_multiplier = 1 if cow_mood != 'upset' else 2
+    less_likely = False if isLucky and cow_mood != 'neutral' else True
+    items = [
+        {"label": "random item", "item": random_item_roll(less_likely), "price_multiplier": 1 * price_multiplier},
+        {"label": "random weapon", "item": build_item('weapon', less_likely=False), "price_multiplier": 1.5 * price_multiplier},
+        {"label": "random shield", "item": build_item('shield', less_likely=False), "price_multiplier": 2 * price_multiplier},
+    ]
+    
+    for item in items:
+        item["price"] = price * item["price_multiplier"]
+
+    return items
+
+def random_tool_roll():
+    tools = [CowBell(), Bucket()]
+    return random.choice(tools)
+
+def random_object_roll():
+    objects = [LiquidGold()]
+    return random.choice(objects)
