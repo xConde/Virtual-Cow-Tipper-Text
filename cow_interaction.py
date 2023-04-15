83d173ebc1a57b5f26f976adb626f41699dd56ef
xConde <edjconde@live.com>
2023-04-14 21:26:22 -0500

    add CowGames

--- cow_interaction.py
+++ cow_interaction.py
@@ -1,7 +1,188 @@
 import random
+import time
+import os
+import msvcrt
 from item import CowBell, Bucket, random_item_roll, get_shop_items
+from assets.context import cow_names
 
+# rarity should raise the floor for items rare dag should not be min 1
 
+# should be able to sell at the shop
+class CowGames:
+    def __init__(self, player, cow):
+        self.player = player
+        self.cow = cow
+
+    def play_random_mini_game(self, bet_amount):
+        mini_games = ['tipping_bar', 'cow_race', 'guessing_game']
+        selected_game = random.choice(mini_games)
+        return getattr(self, selected_game)(bet_amount)
+
+    def tipping_bar(self, bet_amount):
+        max_tip = bet_amount * 3
+        time_interval = max(0.05, 0.3 - 0.01 * bet_amount)
+        return self._tipping_bar(bet_amount, max_tip, time_interval)
+
+    def _tipping_bar(self, tip_amount, max_tip, time_interval):
+        bar_length = 40
+        cow_face_position = random.randint(5, bar_length - 10)
+        target_position = random.randint(5, bar_length - 10)
+
+        while abs(cow_face_position - target_position) < 5:
+            target_position = random.randint(5, bar_length - 10)
+
+        arrow_position = 0
+        direction = 1
+
+        input(f"Tipping Bar | Press ENTER to stop the arrow at the right spot {tip_amount} / {max_tip} | Press ENTER to start!")
+        time.sleep(time_interval)
+
+        while not msvcrt.kbhit():
+            arrow_line = [' '] * bar_length
+            arrow_line[arrow_position] = '>'
+
+            # Cow face rendering
+            cow_face = [
+                '      \\^__^/',
+                '      ( oo )\\________',
+                '        (__)\        )\/\ ',
+                '            ||----w |',
+                '            ||     ||'
+            ]
+            cow_face_width = len(cow_face[0])
+            for i in range(len(cow_face)):
+                cow_face_index = cow_face_position - (cow_face_width // 2) + i
+                if cow_face_index >= 0 and cow_face_index < bar_length:
+                    cow_face_line = list(cow_face[i])
+                    if cow_face_line[0] != ' ':
+                        cow_face_line[0] = '|'
+                    if cow_face_line[-1] != ' ':
+                        cow_face_line[-1] = '|'
+                    cow_face_line = ''.join(cow_face_line)
+                    arrow_line[cow_face_index] = cow_face_line
+
+            # Display the target
+            target = [' '] * bar_length
+            target[target_position - 2:target_position + 2] = '|*|-'
+            print(''.join(target))
+
+            # Display the arrow
+            print(''.join(arrow_line))
+
+            time.sleep(time_interval)
+            arrow_position += direction
+            if arrow_position == 0 or arrow_position == bar_length - 1:
+                direction = -direction
+
+            # Clear the terminal
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        msvcrt.getch()  # clear the key buffer
+
+        # Calculate the tip amount based on whether the arrow_position is within range of the target
+        if target_position - 1 <= arrow_position <= target_position + 1:
+            tip_amount = arrow_position / (bar_length - 1) * max_tip
+        else:
+            tip_amount = 0
+
+        return tip_amount
+    def cow_race(self, bet_amount):
+        race_length = 50
+        base_speed = 1
+        return self._cow_race(race_length, base_speed)
+
+    def _cow_race(self, race_length, base_speed):
+        selected_cow_names = [random.choice(cow_names) for _ in range(3)]
+
+        print("Choose your cow to race with:")
+        for i, name in enumerate(selected_cow_names, start=1):
+            print(f"{i}. {name}")
+
+        while True:
+            try:
+                selected_cow = int(input("Enter the number of your choice (1-3): ")) - 1
+                if 0 <= selected_cow < 3:
+                    break
+                else:
+                    print("Invalid selection. Please enter a number between 1 and 3.")
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and 3.")
+
+        player_cow_position = race_length
+        other_cow_positions = [race_length] * 3
+
+        input("Cow Race | Press ENTER to start!")
+
+        while player_cow_position > 0 and not any(pos <= 0 for pos in other_cow_positions):
+            os.system('cls' if os.name == 'nt' else 'clear')
+            player_cow_position -= base_speed
+            other_cow_positions = [pos - random.randint(1, 3) for pos in other_cow_positions]
+
+            print(f"{'🏁'}{' ' * (race_length - 8)}")
+            print(f"{' ' * (player_cow_position - 1)}🐄 ({selected_cow_names[selected_cow]})")
+            for idx, pos in enumerate(other_cow_positions, start=1):
+                if idx - 1 != selected_cow:
+                    print(f"{' ' * (pos - 1)}🐄 ({selected_cow_names[idx - 1]})")
+
+            time.sleep(0.5)
+
+        if player_cow_position <= 0:
+            print("Congratulations! Your cow won the race!")
+            return self.bet_amount * 2
+        else:
+            print("Your cow lost the race. Better luck next time.")
+            return 0
+
+    def guessing_game(self, tip_amount):
+        secret_number = random.randint(1, int(tip_amount * 1.5))
+        max_guesses = 4
+        range_start, range_end = 1, int(tip_amount * 1.5)
+        return self._guessing_game(tip_amount, secret_number, max_guesses, range_start, range_end)
+
+    def _guessing_game(self, tip_amount, secret_number, max_guesses, range_start, range_end):
+        range_size = range_end - range_start + 1
+        guesses = 0
+        prev_distance = None
+        input(f"Guessing Game | Guess the cow's favorite number between {range_start} and {range_end} | You have {max_guesses} guesses | Press ENTER to start!")
+        while guesses < max_guesses:
+            try:
+                guess = int(input("Enter your guess: "))
+                if guess == secret_number:
+                    print("You guessed the cow's favorite number!")
+                    return tip_amount
+                else:
+                    guesses += 1
+                    remaining_guesses = max_guesses - guesses
+                    if remaining_guesses > 0:
+                        distance = abs(guess - secret_number) / (range_size - 1)
+                        general_hint = ""
+                        if distance < 0.1 and range_end > 40:
+                            general_hint = "You're very hot!"
+                        elif distance < 0.2 and range_end > 20:
+                            general_hint = "You're hot!"
+                        elif distance < 0.3:
+                            general_hint = "You're warm."
+                        elif distance < 0.4:
+                            general_hint = "You're cool."
+                        else:
+                            general_hint = "You're cold."
+                        
+                        warmer_colder_hint = ""
+                        if prev_distance is not None:
+                            if distance < prev_distance:
+                                warmer_colder_hint = "Warmer! "
+                            else:
+                                warmer_colder_hint = "Colder! "
+                        prev_distance = distance
+
+                        print(f"Incorrect! {warmer_colder_hint}{general_hint} You have {remaining_guesses} guesses left.")
+                    else:
+                        print(f"Sorry, you're out of guesses! The correct number was {secret_number}.")
+            except ValueError:
+                print("Please enter a valid number.")
+            except Exception as e:
+                print("An error occurred:", e)
+        return 0
 class CowAttack:
     def __init__(self, name, damage=None, effect=None, duration=None, healing=None, accuracy=100):
         self.name = name
@@ -101,6 +282,7 @@ class CowInteraction():
         else:
             print(f"You encounter a dairy cow named {self.cow.name}, but you don't have a bucket to milk it.")
             self.cow.print_response(self.cow.name, 'dairy_no_bucket')
+        self.game_instance.destroy_cow()
 
     def handle_combat(self):
         print(f"A combat with '{self.cow.name}' has started!")
@@ -116,16 +298,17 @@ class CowInteraction():
                 if choice == 1:
                     self.player.deal_damage(self.cow)
                     if self.cow.hp <= 0:
-                        print(f"You defeat {self.cow.name}.")
-                        self.cow.print_response(self.cow.name, 'enraged_end')
-                        print(f"You gain ${self.cow.cash}.")
+                        self.game_instance.update_cow_scores(self.cow, 1)
                         self.player.cash += self.cow.cash
+                        print(f"You defeat {self.cow.name}. You gain ${self.cow.cash}.")
+                        self.cow.print_response(self.cow.name, 'enraged_end')
                         break
                 elif choice == 2:
                     check_inventory(self.player)
                 elif choice == 3:
                     use_item(self.player)
                 elif choice == 4:
+                    self.game_instance.update_cow_scores(self.cow, -2)
                     print("You flee from the combat.")
                     break
             else:
@@ -136,7 +319,7 @@ class CowInteraction():
                 
     def handle_shop(self):
         print(f'You enter a shop run by a cow named {self.cow.name} who is currently {self.cow.mood}.')
-        self.cow.print_response(self.cow.name, 'shop_keeper_intro')
+        self.cow.print_response(self.cow.name, 'shop_keeper_intro', False)
         isLucky = random.randint(0, 99) < (40 if self.cow.mood == 'upset' else 25)
         if (self.cow.mood == 'friendly' and isLucky):
             print(f"The shop owner is very welcoming and shows you all the items in their shop with a smile.")
@@ -157,14 +340,17 @@ class CowInteraction():
                 choice = int(choice)
                 if choice == 4:
                     self.cow.print_response(self.cow.name, 'shop_keeper_end')
+                    self.game_instance.destroy_cow()
                     return
 
                 item_choice = available_items[choice - 1]
                 item_price = item_choice['price']
                 if self.player.cash >= item_price:
                     self.player.cash -= item_price
+                    score = 1 + int(item_choice['price'] // 50)
+                    self.game_instance.update_cow_scores(self.cow, score)
                     self.player.add_item_to_inventory(item_choice['item'])
-                    self.cow.print_response(self.cow.name, 'shop_keeper_purchase')
+                    self.cow.print_response(self.cow.name, 'shop_keeper_purchase', False)
                     if item_choice['item'].type in ['weapon', 'shield']:
                         print(f"You purchased a {item_choice['item'].stats()} for ${item_price}.")
                     else:
@@ -177,52 +363,63 @@ class CowInteraction():
 
     def handle_tip_or_leave(self):
         print(f'You encounter a cow named {self.cow.name} who is currently {self.cow.mood}.')
-        self.cow.print_response(self.cow.name, 'intro')
-        attempts = 2
-        total_tip = 0
-        while attempts > 0:
-            print("1. Tip the cow | 2. Run")
+        self.cow.print_response(self.cow.name, 'intro', False)
 
-            choice = input("Choose an action (1-2): ")
+        choice = input("Tip or leave? (1. Tip | 2. Leave): ")
+        if choice == "1":
+            min_bet = self.cow.req_amount
+            max_bet = min(self.player.cash, self.cow.req_amount * random.randint(2,5))
 
-            if choice == "1":
+            while True:
                 try:
-                    tip_amount = float(input("How much do you want to tip the cow? "))
-                    if (tip_amount <= 0):
-                        print("You decided not to tip the cow.\n")
-                        attempts = -1
+                    bet_amount = float(input(f"How much do you want to bet for the mini-game? (min: ${min_bet}, max: ${max_bet}) "))
+                    if bet_amount < min_bet:
+                        print(f"Come on, don't be stingy! The cow expects at least ${min_bet}.")
+                    elif bet_amount > max_bet:
+                        print(f"You can't bet more than ${max_bet}, that's the cow's limit.")
                     else:
-                        tip_amount = float(tip_amount)
-                        self.player.cash -= tip_amount
-                        total_tip += tip_amount
-
-                    if tip_amount >= self.cow.req_amount:
-                        score = (2 if attempts == 2 else 1) * (2 if tip_amount > 20 else 1)
-                        self.cow.likeliness += score
-                        self.game_instance.update_cow_scores(self.cow, score)
-                        print(f'You tipped {self.cow.name} ${int(total_tip)}. The cow is happy with your tip. You have ${int(self.player.cash)} left.')
-                        self.cow.print_response(self.cow.name, 'graceful')
-                        return
-                    else:
-                        attempts -= 1
-                        tip_amount = float(tip_amount)
-                        self.player.cash -= tip_amount
-                        print(f"{self.cow.name} is disappointed with your tip{' of $'+str(int(total_tip)) if total_tip > 0 else ''}. You have ${int(self.player.cash)} left.")
-                        if (attempts == 0):
-                            self.cow.likeliness -= 1
-                            self.game_instance.update_cow_scores(self.cow, -1)
-                            print(f"...{self.cow.name} runs away.")
-                        else:
-                             print("...The cow looks at you expectantly, waiting for more money.")
-                        self.cow.print_response(self.cow.name, 'counter')
+                        break
                 except ValueError:
-                    print("Please enter a number.\n")
-            elif choice == "2":
-                print("You decided to run away from the cow.\n")
-                self.game_instance.update_cow_scores(self.cow, -2)
-                return
+                    print("Please enter a valid number.\n")
+
+            self.player.cash -= bet_amount
+            cow_games_instance = CowGames(self.player, self.cow)
+            reward = cow_games_instance.play_random_mini_game(bet_amount)
+
+            if reward > bet_amount:
+                win_amount = round(reward - bet_amount, 2)
+                print(f"Congratulations! You won ${win_amount}!")
+                self.player.cash += reward
+
+                if win_amount >= bet_amount * 1.5:
+                    score = 2
+                elif win_amount >= bet_amount * 0.5:
+                    score = 1
+                else:
+                    score = 0.5
+
+                self.cow.likeliness += score
+                self.game_instance.update_cow_scores(self.cow, score)
+
             else:
-                print("Please enter a number between 1 and 2.")
+                loss_amount = bet_amount - reward
+                self.player.cash += reward
+
+                if loss_amount >= bet_amount * 0.5:
+                    score = -0.5
+                else:
+                    score = -1
+
+                self.cow.likeliness += score
+                self.game_instance.update_cow_scores(self.cow, score)
+
+        elif choice == "2":
+            print(f"You decided to leave {self.cow.name}.")
+            score = -2
+            self.cow.likeliness += score
+            self.game_instance.update_cow_scores(self.cow, score)
+        else:
+            print("Please enter a number between 1 and 2.")
 
     def get_cow_bell(self):
         for item in self.player.inventory:
