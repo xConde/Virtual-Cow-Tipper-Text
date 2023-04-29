b5ab60e0fcf0e1b4472617f8cfe736c033ad54ea
xConde <edjconde@live.com>
2023-04-28 22:42:13 -0500

    prevent redundant context in dialog_history

--- terminal/dialog_history.py
+++ terminal/dialog_history.py
@@ -7,6 +7,9 @@ class DialogHistory:
 
     def add_dialog(self, message):
         timestamp = datetime.now().strftime("%b %d | %I:%M%p")
+        if self.dialog_history and self.dialog_history[-1] == (timestamp, message):
+            return
+            
         self.dialog_history.append((timestamp, message))
         if len(self.dialog_history) > self.dialog_max_length:
             self.dialog_history.pop(0)
